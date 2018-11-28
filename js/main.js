0bb40f249b1c90e66e57949752cb734608396f8f
Joanna Gammel <jgammel@berklee.edu>
2018-11-27 21:45:41 -0500

    LED prototype

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var mic;
 var fft;
-var div = document.getElementById('output');
-
+var led_R;
+var led_O;
+var led_Y;
+var led_G;
+var led_B;
+var led_V;
+var led_P;
 
 function setup() {
+  createCanvas(700, 700);
+
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -13,40 +20,61 @@ function setup() {
   // By default, it does not .connect() (to the computer speakers)
   mic.start();
   fft.setInput(mic);
+}
+
+function draw() {
+   background(300);
   
-}
 
-var spectrum = fft.analyze();
+  // led_R = rect(0, 0, 100, 100);
+  // led_O = rect(100, 0, 100, 100);
+  // led_Y = rect(200, 0, 100, 100);
+  // led_G = rect(300, 0, 100, 100);
+  // led_B = rect(400, 0, 100, 100);
+  // led_V = rect(500, 0, 100, 100);
+  // led_P = rect(600, 0, 100, 100);
 
-//STATEMENT STARTS HERE//
+   var spectrum = fft.analyze();
+
+  //beginShape();
+
+   var largestHz = spectrum.indexOf(Math.max.apply(Math, spectrum)); // clearest frequency picked up by the microphone
+
+
+     //STATEMENT STARTS HERE//
+
+    if(largestHz % 7 == 0){
+    	fill(255,0,0);
+    	rect(0, 0, 100, 100);
+	}
+    else if(largestHz % 6 == 0){
+    	fill(255,135,55);
+    	rect(100, 0, 100, 100);
+    }
+    else if(largestHz % 5 == 0){
+        fill(255,214,62);
+        rect(200, 0, 100, 100);
+    }
+    else if(largestHz % 4 == 0){
+        fill(0,220,62);
+        rect(300, 0, 100, 100);
+    }
+    else if(largestHz % 3 == 0){
+        fill(0,148,234);
+        rect(400, 0, 100, 100);
+    }
+    else if(largestHz % 2 == 0){
+        fill(108,0,234);
+        rect(500, 0, 100, 100);
+    }
+    else {
+        fill(255,108,234);
+        rect(600, 0, 100, 100);
+    }
+
      
-if(spectrum[i] > 0){
-  switch(i % 7){
-    case 0:
-      div.innerHTML += 'red' + '<br/>';
-      break;
-    case 1:
-      div.innerHTML += 'orange' + '<br/>';
-       break;
-    case 2:
-      div.innerHTML += 'yellow' + '<br/>';
-      break;
-    case 3:
-      div.innerHTML += 'green' + '<br/>';
-      break;
-    case 4:
-      div.innerHTML += 'blue' + '<br/>';
-      break;
-    case 5:
-      div.innerHTML += 'violet' + '<br/>';
-      break;
-    case 6:
-      div.innerHTML += 'pink' + '<br/>';
-      break;
-    default:
-      console.log("unknown tone");
+     //STATEMENT ENDS HERE//
      
-  }
+  
+   //endShape();
 }
-
-//STATEMENT ENDS HERE//
